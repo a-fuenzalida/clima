f8da096cc30051fe31a428ce0522c6933af9a377
a-fuenzalida <a.fuenzalida1494@gmail.com>
2019-12-07 12:22:34 -0300

    Updates

---
+++ precache-manifest.0ef888a0e5f3b7e41ec380d81e3b2b63.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98cd59e617490d8761a9788ab03f3663",
+    "url": "/clima/index.html"
+  },
+  {
+    "revision": "9c840478e8af0c573a1d",
+    "url": "/clima/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "ecea105dd8da47ba7102",
+    "url": "/clima/static/js/2.49af0c32.chunk.js"
+  },
+  {
+    "revision": "0c559e1ae0a9426ead1485b442d61f0b",
+    "url": "/clima/static/js/2.49af0c32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c840478e8af0c573a1d",
+    "url": "/clima/static/js/main.9638b3ae.chunk.js"
+  },
+  {
+    "revision": "df1c1c8ea8193d3bfd76",
+    "url": "/clima/static/js/runtime-main.f845c372.js"
+  },
+  {
+    "revision": "74cf5f47bd928217848dd46f1ad3bfb6",
+    "url": "/clima/static/media/ClearDay.74cf5f47.svg"
+  },
+  {
+    "revision": "b46c48927e92c8b74ebf99ca623998e3",
+    "url": "/clima/static/media/ClearNight.b46c4892.svg"
+  },
+  {
+    "revision": "3582b69f733b14823ced29599a37710f",
+    "url": "/clima/static/media/CloudsDay.3582b69f.svg"
+  },
+  {
+    "revision": "2d1dd239fee99c64039a2723d809c5ca",
+    "url": "/clima/static/media/CloudsNight.2d1dd239.svg"
+  },
+  {
+    "revision": "3fde5592050adeb2a8956c2df809ccba",
+    "url": "/clima/static/media/Drizzle.3fde5592.svg"
+  },
+  {
+    "revision": "cd97be3071367a0b045392b9e92a6be8",
+    "url": "/clima/static/media/Rain.cd97be30.svg"
+  },
+  {
+    "revision": "58961232e9f21d20f895e6f417bfc003",
+    "url": "/clima/static/media/Snow.58961232.svg"
+  },
+  {
+    "revision": "886fe4ff6a6713e915c229ff5bc5e871",
+    "url": "/clima/static/media/Thunderstorm.886fe4ff.svg"
+  }
+]);
